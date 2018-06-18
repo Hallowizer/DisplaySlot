a890608397bacf3ea030175210771a46b42e5000
Hallowizer <40325350+Hallowizer@users.noreply.github.com>
2018-06-17 22:13:16 -0700

    Delete PluginContext.java

--- displayslot-api-loader/src/main/java/com/hallowizer/displaySlot/plugin/apiLoader/PluginContext.java
+++
@@ -1,9 +0,0 @@
-package com.hallowizer.displaySlot.apiLoader;
-
-import java.util.UUID;
-
-public interface PluginContext {
-	public void sendPluginMessage(UUID uuid, String channel, byte[] data);
-	public int getScreenWidth(UUID uuid);
-	public int getScreenHeight(UUID uuid);
-}
